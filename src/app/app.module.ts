905cfee64935c0d4b1cb1b9714387bd875d7c715
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2021-12-13 08:14:39 +0800

    Initial user menu implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,29 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/* Angular Material Modules */
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
 /* Add Amplify imports */
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 import Amplify from 'aws-amplify';
 import awsconfig from '../aws-exports';
+
+
+/* internal components */
 import { SidenavComponent } from './common/sidenav/sidenav.component';
 import { MainComponent } from './layouts/main/main.component';
-import { MatIconModule } from '@angular/material/icon';
 import { DashboardComponent } from './views/dashboard.component';
 import { ProjectsComponent } from './views/projects.component';
+import { UserMenuComponent } from './common/user-menu/user-menu.component';
+
 
 /* Configure Amplify resources */
 Amplify.configure(awsconfig);
 
 @NgModule({
-  declarations: [AppComponent, SidenavComponent, MainComponent, DashboardComponent, ProjectsComponent],
+  declarations: [AppComponent, SidenavComponent, MainComponent, DashboardComponent, ProjectsComponent, UserMenuComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -28,6 +36,7 @@ Amplify.configure(awsconfig);
     BrowserAnimationsModule,
     AmplifyAuthenticatorModule,
     MatIconModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/common/user-menu/user-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-user-menu',
+  templateUrl: './user-menu.component.html',
+  styleUrls: ['./user-menu.component.scss']
+})
+export class UserMenuComponent {
+  @Input() user: any;
+  @Output() signout: EventEmitter<any> = new EventEmitter();
+  @Output() changePassword: EventEmitter<any> = new EventEmitter();
+  @Output() profile: EventEmitter<any> = new EventEmitter();
+
+  constructor() { }
+}
