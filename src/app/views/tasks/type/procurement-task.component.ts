a695e83941a955fce9f82d3fdd6fe2514d096767
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2022-03-17 21:30:57 +0800

    Initial procurement task implementation w/ dummy data

--- src/app/views/tasks/type/procurement-task.component.ts
+++ src/app/views/tasks/type/procurement-task.component.ts
@@ -1,15 +1,78 @@
 import { Component, OnInit } from '@angular/core';
+import { ScopeOfWork } from 'src/app/models/scope-of-work.model';
 
 @Component({
   selector: 'app-procurement-task',
   templateUrl: './procurement-task.component.html',
-  styleUrls: ['./procurement-task.component.scss']
+  styleUrls: ['./procurement-task.component.scss'],
 })
 export class ProcurementTaskComponent implements OnInit {
+  scopes: ScopeOfWork[] = [
+    {
+      name: 'EARTHWORK',
+      tasks: [
+        {
+          name: 'Demotion Works',
+          unit: 'LOT',
+          quantity: 1.0,
+          materials: [],
+        },
+        {
+          name: 'Excavation Works',
+          unit: 'cu.m.',
+          quantity: 112.98,
+          materials: [],
+        },
+        {
+          name: 'Backfilling',
+          unit: 'cu.m.',
+          quantity: 210.74,
+          materials: [],
+        },
+      ],
+    },
+    {
+      name: 'RETAINING WALL',
+      tasks: [
+        {
+          name: 'Reinforcement Bars',
+          materials: [
+            {
+              name: 'Footing RSB 25mm x 9mm',
+              unit: 'pcs',
+              quantity: 20.0,
+              contingency: 5,
+            },
+            {
+              name: 'Footing RSB 20mm x 6mm',
+              unit: 'pcs',
+              quantity: 42.0,
+              contingency: 5,
+            },
+            {
+              name: 'Footing RSB 16mm x 6mm',
+              unit: 'pcs',
+              quantity: 240.0,
+              contingency: 5,
+            },
+          ],
+        },
+        {
+          name: 'Formworks',
+          materials: [
+            {
+              name: 'Phenolic Board 3/4',
+              unit: 'pcs',
+              quantity: 1,
+              contingency: 5,
+            },
+          ],
+        },
+      ],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
