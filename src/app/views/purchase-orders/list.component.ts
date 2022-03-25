f9751eb6da966400d11724395324043b3b435213
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2022-03-26 01:48:48 +0800

    Implemnet PO list

--- src/app/views/purchase-orders/list.component.ts
+++ src/app/views/purchase-orders/list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataTableColumnDef } from 'src/app/common/data-table/data-table.component';
+import { ActionDataTableRendererComponent } from 'src/app/common/data-table/renderers/action.component';
+import { DateDataTableRendererComponent } from 'src/app/common/data-table/renderers/date.component';
 
 @Component({
   selector: 'app-list',
@@ -6,7 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss'],
 })
 export class PurchaseOrderListComponent implements OnInit {
-  constructor() {}
+  columns: DataTableColumnDef[] = [
+    { id: 'vendor', label: 'Vendor' },
+    { id: 'total', label: 'Total' },
+    {
+      id: 'date',
+      label: 'Date',
+      width: '10%',
+      renderer: DateDataTableRendererComponent,
+    },
+    {
+      id: 'action',
+      label: 'Action',
+      width: '10%',
+      renderer: ActionDataTableRendererComponent,
+    },
+  ];
+
+  constructor(public router: Router) {}
 
   ngOnInit(): void {}
 }
