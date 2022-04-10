29e7dd050dd40467f7b8100cca09a6a1335c555e
James Kenneth Guidaven <jkguidaven@gmail.com>
2022-04-11 01:09:48 +0800

    Add checking for accessing project pages

--- src/app/views/projects.component.ts
+++ src/app/views/projects.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { CognitoUserSession } from 'amazon-cognito-identity-js';
+import { Auth } from 'aws-amplify';
+import { from, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
-  constructor() { }
+  hasAccess: boolean = false;
+  constructor() {}
 
   ngOnInit(): void {
+    this.getAuth().subscribe((session) => {
+      const groups = session.getIdToken().payload['cognito:groups'];
+      this.hasAccess = groups.indexOf('admin') > -1;
+    });
+  }
+
+  private getAuth(): Observable<CognitoUserSession> {
+    return from(Auth.currentSession());
   }
 }
